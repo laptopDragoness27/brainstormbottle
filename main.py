8689b38d44ea06b3b55db5ec6a3d4d7503a0ee9c
Evelyn Langlais <cflanglais10@gmail.com>
2026-07-01 08:33:53 -0400

    centred all the widgets and made the text entry box wider

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 
 root = tk.Tk()
-root.geometry('300x200')
+root.geometry('800x600')
 root.title('Brainstorm in a Bottle')
 
 ideaslist = []
@@ -10,13 +10,14 @@ ideasSym = []
 
 def result():
     ideaslist.append(email_entry.get())
-    ideasSym.append(ttk.Label(root,text=ideaslist[-1]).pack(pady=5))
-    print(ideaslist)
+    ideasSym.append(ttk.Label(root,text=ideaslist[-1]).grid(column=0,row=len(ideaslist)+1))
 
 
-email_entry = ttk.Entry(root)
-email_entry.pack(pady=5)
+email_entry = ttk.Entry(root, width=80)
+email_entry.grid(column=0,row=0,sticky="nsew")
 
-enterButton = ttk.Button(root, text="enter", command=result).pack(pady=5)
+enterButton = ttk.Button(root, text="enter", command=result).grid(column=0,row=1,sticky="nsew")
+
+root.grid_columnconfigure(0, weight=1)
 
 root.mainloop()
